a96f0fadc118c7ac99517e6053bbde4af414b3fb
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-02-28 16:55:23 +0530

    feat: show Ui can read data

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,10 +3,12 @@
 
 <script>
 
+    var note = "";
+
     import "../app.css";
     function saveNote() {
         // TODO
-        alert("TODO ⚒️");
+        alert("TODO (implement API and etc stuff) ⚒️ => Your Note: " + note);
     }
 
 </script>
@@ -55,7 +57,7 @@
 
             <div class="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800">
                 <label for="editor" class="sr-only">Publish post</label>
-                <textarea id="editor" rows="18" 
+                <textarea bind:value={note} id="editor" rows="18" 
                     class="block w-full px-0 text-sm text-gray-800 bg-white border-1 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400" placeholder="Write an article..." required></textarea>
             </div>
         </div>
