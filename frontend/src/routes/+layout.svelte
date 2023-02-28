6e86523ce3d71b81078e7670fcdb14fed711a2a0
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-02-28 16:32:30 +0530

    feat: some ui improvements

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<div class="h-screen bg-white dark:bg-gray-900 px-5">
+    <header>
+    </header>
+    
+    <main class="">
+        <slot />
+    </main>
+    
+</div>
+
