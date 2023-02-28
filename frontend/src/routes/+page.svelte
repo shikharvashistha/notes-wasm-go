0b3b23bac457082ab16ad187c9969e02e8dfb68e
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-02-28 15:32:05 +0530

    feat: Add some UI

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,28 +2,55 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 
 <script>
-    var note = "";
 
-    function isEmpty() {
-        if (note == "") {
-            return true;
-        }
-        return false;
-    }
-
+    import "../app.css";
     function saveNote() {
         // TODO
-        alert("TODO");
+        alert("TODO ⚒️");
     }
+
 </script>
 
-<div>
-    <input type="button"
-        on:click={saveNote}
-        value="Cloud Save">
-        
-    <textarea bind:value={note} placeholder="Enter your notes"></textarea>    
-    <button type="submit">Submit</button>
-    
-    <p>: {note}</p>
+<div class="h-screen bg-white dark:bg-gray-900 px-2">
+<h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tigh text-center text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Notes</h1>
+
+<form>
+    <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+        <div class="flex items-center justify-between px-3 py-2 border-b dark:border-gray-600">
+            <div class="flex flex-wrap items-center divide-gray-200 sm:divide-x dark:divide-gray-600">
+                <div class="flex items-center space-x-1 sm:pr-4">
+                    <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd"></path></svg>
+                        <span class="sr-only">Attach file</span>
+                    </button>
+                    <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path></svg>
+                        <span class="sr-only">Embed map</span>
+                    </button>
+                    <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd"></path></svg>
+                        <span class="sr-only">Upload image</span>
+                    </button>
+                    <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                         <span class="sr-only">Format code</span>
+                    </button>
+                    <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z" clip-rule="evenodd"></path></svg>
+                        <span class="sr-only">Add emoji</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800">
+            <label for="editor" class="sr-only">Publish post</label>
+            <textarea id="editor" rows="8" 
+                class="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write an article..." required></textarea>
+        </div>
+    </div>
+    <button type="submit" on:click={saveNote} class="inline-flex items-center place-items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+        Add Note
+    </button>
+ </form>
+
 </div>
