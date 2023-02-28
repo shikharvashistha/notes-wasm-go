c1222dac2c1bfac353f4fdc68617ac97dbfbdde2
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-02-28 08:09:26 +0530

    Tidy up svelte and some skeleton layer work

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<script>
+    var note = "";
+
+    function isEmpty() {
+        if (note == "") {
+            return true;
+        }
+        return false;
+    }
+
+    function saveNote() {
+        // TODO
+        alert("TODO");
+    }
+</script>
+
+<div>
+    <input type="button"
+        on:click={saveNote}
+        value="Cloud Save">
+        
+    <textarea bind:value={note} placeholder="Enter your notes"></textarea>    
+    <button type="submit">Submit</button>
+    
+    <p>: {note}</p>
+</div>
