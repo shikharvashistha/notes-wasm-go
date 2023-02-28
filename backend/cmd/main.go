757f10b98df1462a222fdd4abe1b69c1c1b39694
Shikhar Vashistha <51105234+shikharvashistha@users.noreply.github.com>
2023-02-28 06:06:51 +0000

    chore: added error handling

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"net/http"
 	"os"
 
@@ -10,23 +9,38 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
+	w.Write([]byte("Running"))
 	r.Write(w)
 }
 
 func main() {
-	wasmBytes, _ := os.ReadFile("/workspace/notes-wasm-go/backend/wasm/main.wasm")
+	//wasmBytes, _ := ioutil.ReadFile("/workspace/notes-wasm-go/backend/wasm/main.wasm")
+	wasmBytes, err := os.ReadFile("/workspace/notes-wasm-go/backend/wasm/main.wasm")
+	if err != nil {
+		logger.Warn("Error reading wasm file")
+	}
 	engine := wasmer.NewEngine()
 	store := wasmer.NewStore(engine)
-	module, _ := wasmer.NewModule(store, wasmBytes)
+	module, err := wasmer.NewModule(store, wasmBytes)
+	if err != nil {
+		logger.Warn("Error creating module")
+	}
 	importObject := wasmer.NewImportObject()
-	instance, _ := wasmer.NewInstance(module, importObject)
+	instance, err := wasmer.NewInstance(module, importObject)
+	if err != nil {
+		logger.Warn("Error creating instance")
+	}
 
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/encrypt", func(w http.ResponseWriter, r *http.Request) {
 		encrypt, _ := instance.Exports.GetFunction("EncryptNotes")
-		notesBody, _ := io.ReadAll(r.Body)
-		key, err := encrypt(notesBody)
+		notesBytes := make([]byte, 1000)
+		notesBody, err := r.Body.Read(notesBytes)
+		if err != nil {
+			logger.Warn("Error reading notes")
+		}
+		notes := string(notesBytes[:notesBody])
+		key, err := encrypt(notes)
 		if err != nil {
 			logger.Warn("Error encrypting notes")
 		}
@@ -37,8 +51,14 @@ func main() {
 
 	http.HandleFunc("/decrypt", func(w http.ResponseWriter, r *http.Request) {
 		decrypt, _ := instance.Exports.GetFunction("DecryptNotes")
-		notesBody, _ := io.ReadAll(r.Body)
-		notes, err := decrypt(notesBody)
+		notesBytes := make([]byte, 1000)
+		notesBody, err := r.Body.Read(notesBytes)
+		if err != nil {
+			logger.Warn("Error reading notes")
+		}
+		notesString := string(notesBytes[:notesBody])
+		key := string(r.Header.Get("key"))
+		notes, err := decrypt(notesString, key)
 		if err != nil {
 			logger.Warn("Error decrypting notes")
 		}
