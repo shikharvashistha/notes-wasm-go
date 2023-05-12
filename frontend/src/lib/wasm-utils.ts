30f7de351967ce9d6b49a7eca6c1a8750d4dc4c3
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-05-12 12:02:33 +0530

    feat: cleanup

--- frontend/src/lib/wasm-utils.ts
+++ frontend/src/lib/wasm-utils.ts
@@ -5,19 +5,6 @@
 import "$lib/wasm_exec.js";
 import wasm from "$lib/main.wasm?url";
 
-declare function git_clone(url: string): Promise<string>;
-declare function touchNcat(path: string, content: string): Promise<string>;
-
-interface push {
-  url: string;
-  AccessTocken: string;
-  userName: string;
-  userEmail: string;
-  fileName: string;
-  commitMessage: string;
-}
-declare function git_push(push: push): Promise<string>;
-
 const wasm_init = async () => {
   // @ts-ignore
   const go = new Go(); // defined in wasm_exec.js
